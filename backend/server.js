22654b8ab656df0e6e0f09a0fc23bf4295d9b5e8
venkatanarasimhavalavala <valavalavenkatanarasimha@gmail.com>
2026-01-26 14:26:23 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.use("/api/doctor", doctorRoutes);
 app.use("/api/operator", operatorRoutes);
 
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
